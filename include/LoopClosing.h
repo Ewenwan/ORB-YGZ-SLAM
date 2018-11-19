3d6c70af7df91b49bc4ebfdfb12111f151ecdadb
Wan Youwen <1756429307@qq.com>
2018-11-19 21:48:40 +0800

    回环检测=Loop closing 线程 加IMU部分

--- include/LoopClosing.h
+++ include/LoopClosing.h
@@ -1,21 +1,6 @@
 /**
 * This file is part of ORB-SLAM2.
-*
-* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
-* For more information see <https://github.com/raulmur/ORB_SLAM2>
-*
-* ORB-SLAM2 is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM2 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+*  回环检测=Loop closing 线程
 */
 
 #ifndef YGZ_LOOPCLOSING_H_
@@ -31,68 +16,68 @@
 namespace ygz {
 
     class Tracking;
-
     class LocalMapping;
-
     class KeyFrameDatabase;
 
-    class KeyFrame;
-
-    class Map;
-
-    class MapPoint;
+    class KeyFrame;//========
+    class Map;//===
+    class MapPoint;//==
 
     class LoopClosing {
     public:
+
         EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-        typedef pair<set<KeyFrame *>, int> ConsistentGroup;
-        typedef map<KeyFrame *, g2o::Sim3, std::less<KeyFrame *>,
-                Eigen::aligned_allocator<std::pair<const KeyFrame *, g2o::Sim3> > > KeyFrameAndPose;
 
-        LoopClosing(Map *pMap, KeyFrameDatabase *pDB, ORBVocabulary *pVoc, const bool bFixScale, ConfigParam *pParams);
+        typedef pair<set<KeyFrame *>, int> ConsistentGroup; // 具有连续性的候选帧
+        typedef map<KeyFrame *, 
+                                     g2o::Sim3, 
+                                     std::less<KeyFrame *>,
+                                     Eigen::aligned_allocator<std::pair<const KeyFrame *,
+                                                                                                       g2o::Sim3> > 
+                                    >  KeyFrameAndPose;
 
-        void SetTracker(Tracking *pTracker);
+        LoopClosing(Map *pMap, KeyFrameDatabase *pDB, 
+                                  ORBVocabulary *pVoc, const bool bFixScale, ConfigParam *pParams);
 
-        void SetLocalMapper(LocalMapping *pLocalMapper);
+        void SetTracker(Tracking *pTracker); // 跟踪器====
+
+        void SetLocalMapper(LocalMapping *pLocalMapper);// 局部建图=====
 
         // Main function
-        void Run();
+        void Run();//Loop closing 线程 主函数====
 
-        void InsertKeyFrame(KeyFrame *pKF);
+        void InsertKeyFrame(KeyFrame *pKF);// 插入关键帧====
 
-        void RequestReset();
+        void RequestReset();// 请求重置====
 
-        // This function will run in a separate thread
+        // This function will run in a separate thread  全局优化=====
         void RunGlobalBundleAdjustment(unsigned long nLoopKF);
 
         bool isRunningGBA() {
             unique_lock<std::mutex> lock(mMutexGBA);
-            return mbRunningGBA;
+            return mbRunningGBA; // 全局优化 是否在运行===
         }
 
         bool isFinishedGBA() {
             unique_lock<std::mutex> lock(mMutexGBA);
-            return mbFinishedGBA;
+            return mbFinishedGBA;// 全局优化 是否 结束===
         }
 
-        void RequestFinish();
+        void RequestFinish();// 请求完成====
 
-        bool isFinished();
+        bool isFinished();// 回环检测是否完成
 
+// IMU 参数=======================================
         ConfigParam *mpParams;
         bool mbUseIMU;
-
         bool GetMapUpdateFlagForTracking();
-
         void SetMapUpdateFlagInTracking(bool bflag);
 
     protected:
 
         bool CheckNewKeyFrames();
-
-        bool DetectLoop();
-
-        bool ComputeSim3();
+        bool DetectLoop();// 检测闭环==================
+        bool ComputeSim3();// 计算闭环处的相似变换=====
 
         void SearchAndFuse(const KeyFrameAndPose &CorrectedPosesMap);
 
@@ -119,7 +104,7 @@ namespace ygz {
 
         LocalMapping *mpLocalMapper;
 
-        std::list<KeyFrame *> mlpLoopKeyFrameQueue;
+        std::list<KeyFrame *> mlpLoopKeyFrameQueue; // 闭环帧队列=======
 
         std::mutex mMutexLoopQueue;
 
@@ -129,11 +114,11 @@ namespace ygz {
         // Loop detector variables
         KeyFrame *mpCurrentKF;
         KeyFrame *mpMatchedKF;
-        std::vector<ConsistentGroup> mvConsistentGroups;
+        std::vector<ConsistentGroup> mvConsistentGroups;// 具有连续性的候选帧 群组
         std::vector<KeyFrame *> mvpEnoughConsistentCandidates;
-        std::vector<KeyFrame *> mvpCurrentConnectedKFs;
-        std::vector<MapPoint *> mvpCurrentMatchedPoints;
-        std::vector<MapPoint *> mvpLoopMapPoints;
+        std::vector<KeyFrame *> mvpCurrentConnectedKFs;// 当前帧相连的 关键帧
+        std::vector<MapPoint *> mvpCurrentMatchedPoints;//当前帧 闭环检测得到的 匹配地图点
+        std::vector<MapPoint *> mvpLoopMapPoints;// 当前帧 相邻关键帧上的 地图点 闭环处的地图点
         cv::Mat mScw;
         g2o::Sim3 mg2oScw;
 
@@ -147,9 +132,10 @@ namespace ygz {
         std::thread *mpThreadGBA;
 
         // Fix scale in the stereo/RGB-D case
-        bool mbFixScale;
+        bool mbFixScale;  // 双目/深度 固定的 空间尺度
         bool mnFullBAIdx;
 
+// ===================add 
         std::mutex mMutexMapUpdateFlag;
         bool mbMapUpdateFlagForTracking = false;
     };
