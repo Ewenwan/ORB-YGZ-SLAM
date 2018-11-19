c3b36d71737fb4563927a5d9592a49a8e49d9051
Wan Youwen <1756429307@qq.com>
2018-11-19 20:03:26 +0800

    关键帧显示

--- include/FrameDrawer.h
+++ include/FrameDrawer.h
@@ -1,21 +1,6 @@
 /**
 * This file is part of ORB-SLAM2.
-*
-* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
-* For more information see <https://github.com/raulmur/ORB_SLAM2>
-*
-* ORB-SLAM2 is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM2 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+* 关键帧显示
 */
 
 #ifndef YGZ_FRAMEDRAWER_H_
@@ -29,9 +14,8 @@
 
 namespace ygz {
 
-    class Tracking;
-
-    class Viewer;
+    class Tracking;// 声明跟踪类
+    class Viewer;   // 声明 可视化类
 
     class FrameDrawer {
     public:
@@ -41,11 +25,11 @@ namespace ygz {
         void Update(Tracking *pTracker);
 
         // Draw last processed frame.
-        cv::Mat DrawFrame();
+        cv::Mat DrawFrame();// 显示上一帧
 
     protected:
 
-        void DrawTextInfo(cv::Mat &im, int nState, cv::Mat &imText);
+        void DrawTextInfo(cv::Mat &im, int nState, cv::Mat &imText);//显示 文本信息
 
         // Info of the frame to be drawn
         cv::Mat mIm;
@@ -57,7 +41,7 @@ namespace ygz {
         vector<cv::KeyPoint> mvIniKeys;
         vector<int> mvIniMatches;
         int mState;
-        vector<int> mvMatchedFrom;
+        vector<int> mvMatchedFrom;// 标识每个特征点是从哪个Keyframe选取的========
 
         Map *mpMap;
 
@@ -66,4 +50,4 @@ namespace ygz {
 
 } //namespace ygz
 
-#endif // FRAMEDRAWER_H
+#endif // YGZ_FRAMEDRAWER_H
