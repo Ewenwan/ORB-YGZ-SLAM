43b9358e9a8337a8b9de997900b9e30a40a7e191
Wan Youwen <1756429307@qq.com>
2018-11-20 11:47:48 +0800

    * 全局/局部 优化 使用G2O图优化   IMU全局、局部、本征图、相似变换、单目深度...优化

--- include/Optimizer.h
+++ include/Optimizer.h
@@ -1,21 +1,7 @@
 /**
 * This file is part of ORB-SLAM2.
-*
-* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
-* For more information see <https://github.com/raulmur/ORB_SLAM2>
-*
-* ORB-SLAM2 is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM2 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+* 全局/局部 优化 使用G2O图优化
+  IMU全局、局部、本征图、相似变换、单目深度...优化
 */
 
 #ifndef YGZ_OPTIMIZER_H_
@@ -36,21 +22,37 @@ namespace ygz {
 
     class Optimizer {
     public:
-        // IMU related
-        void static GlobalBundleAdjustmentNavState(Map *pMap, const Vector3d &gw, int nIterations, bool *pbStopFlag,
-                                                   const unsigned long nLoopKF, const bool bRobust);
+
+        // IMU 相关===============导航状态全局BA优化=========================
+        void static GlobalBundleAdjustmentNavState(
+                                                  Map *pMap, 
+                                                  const Vector3d &gw, 
+                                                  int nIterations, 
+                                                  bool *pbStopFlag,
+                                                  const unsigned long nLoopKF, 
+                                                  const bool bRobust);
 
         int static
-        PoseOptimization(Frame *pFrame, KeyFrame *pLastKF, const IMUPreintegrator &imupreint, const Vector3d &gw,
-                         const bool &bComputeMarg = false);
+        PoseOptimization(Frame *pFrame, 
+                                            KeyFrame *pLastKF, 
+                                            const IMUPreintegrator &imupreint, 
+                                            const Vector3d &gw,
+                                            const bool &bComputeMarg = false);
 
         int static
-        PoseOptimization(Frame *pFrame, Frame *pLastFrame, const IMUPreintegrator &imupreint, const Vector3d &gw,
-                         const bool &bComputeMarg = false);
-
+        PoseOptimization(Frame *pFrame, 
+                                             Frame *pLastFrame, 
+                                             const IMUPreintegrator &imupreint, 
+                                             const Vector3d &gw,
+                                             const bool &bComputeMarg = false);
+        // =导航状态局部BA优化=========================
         void static
-        LocalBundleAdjustmentNavState(KeyFrame *pKF, const std::list<KeyFrame *> &lLocalKeyFrames, bool *pbStopFlag,
-                                      Map *pMap, const Vector3d &gw, LocalMapping *pLM = NULL);
+        LocalBundleAdjustmentNavState(KeyFrame *pKF, 
+                                                                           const std::list<KeyFrame *> &lLocalKeyFrames, 
+                                                                           bool *pbStopFlag,
+                                                                           Map *pMap, 
+                                                                           const Vector3d &gw, 
+                                                                          LocalMapping *pLM = NULL);
 
         Vector3d static OptimizeInitialGyroBias(const std::list<KeyFrame *> &lLocalKeyFrames);
 
@@ -58,30 +60,53 @@ namespace ygz {
 
         Vector3d static OptimizeInitialGyroBias(const std::vector<Frame> &vFrames);
 
-        Vector3d static OptimizeInitialGyroBias(const vector<SE3d> &vTwc, const vector<IMUPreintegrator> &vImuPreInt);
+        Vector3d static OptimizeInitialGyroBias(const vector<SE3d> &vTwc, 
+                                                                                         const vector<IMUPreintegrator> &vImuPreInt);
 
-        // vision only
-        void static BundleAdjustment(const std::vector<KeyFrame *> &vpKF, const std::vector<MapPoint *> &vpMP,
-                                     int nIterations = 5, bool *pbStopFlag = NULL, const unsigned long nLoopKF = 0,
-                                     const bool bRobust = true);
 
-        void static GlobalBundleAdjustemnt(Map *pMap, int nIterations = 5, bool *pbStopFlag = NULL,
-                                           const unsigned long nLoopKF = 0, const bool bRobust = true);
-
-        void static LocalBundleAdjustment(KeyFrame *pKF, bool *pbStopFlag, Map *pMap, LocalMapping *pLM = NULL);
-
+        // 视觉相关的 优化===========================================================
+        void static BundleAdjustment(const std::vector<KeyFrame *> &vpKF, 
+                                                                       const std::vector<MapPoint *> &vpMP,
+                                                                       int nIterations = 5, 
+                                                                       bool *pbStopFlag = NULL, 
+                                                                       const unsigned long nLoopKF = 0,
+                                                                       const bool bRobust = true);
+        // 全局BA优化===
+        void static GlobalBundleAdjustemnt(Map *pMap,
+                                                                                   int nIterations = 5, 
+                                                                                   bool *pbStopFlag = NULL,
+                                                                                   const unsigned long nLoopKF = 0, 
+                                                                                   const bool bRobust = true);
+       // 局部BA优化====
+        void static LocalBundleAdjustment(KeyFrame *pKF, 
+                                                                                  bool *pbStopFlag, 
+                                                                                  Map *pMap, 
+                                                                                  LocalMapping *pLM = NULL);
+         // 位姿 优化====
         int static PoseOptimization(Frame *pFrame);
-
+         
+        // 本征图，所有节点的最小生成树图，全局优化=========
+        // 优化图 3d位姿 + 3d平移  双目/深度 优化6维
+        // 单目 在优化 3d位姿 + 3d平移上 再 优化一维 深度信息
         // if bFixScale is true, 6DoF optimization (stereo,rgbd), 7DoF otherwise (mono)
-        void static OptimizeEssentialGraph(Map *pMap, KeyFrame *pLoopKF, KeyFrame *pCurKF,
+        void static OptimizeEssentialGraph(
+                                           Map *pMap, 
+                                           KeyFrame *pLoopKF, 
+                                           KeyFrame *pCurKF,
                                            const LoopClosing::KeyFrameAndPose &NonCorrectedSim3,
                                            const LoopClosing::KeyFrameAndPose &CorrectedSim3,
                                            const map<KeyFrame *, set<KeyFrame *> > &LoopConnections,
                                            const bool &bFixScale, LoopClosing *pLC = NULL);
 
         // if bFixScale is true, optimize SE3 (stereo,rgbd), Sim3 otherwise (mono)
-        static int OptimizeSim3(KeyFrame *pKF1, KeyFrame *pKF2, std::vector<MapPoint *> &vpMatches1,
-                                g2o::Sim3 &g2oS12, const float th2, const bool bFixScale);
+       // 相似变换优化=================================
+        static int OptimizeSim3(
+                                KeyFrame *pKF1, 
+                                KeyFrame *pKF2, 
+                                std::vector<MapPoint *> &vpMatches1,
+                                g2o::Sim3 &g2oS12, 
+                                const float th2, 
+                                const bool bFixScale);
     };
 
 } //namespace ygz
